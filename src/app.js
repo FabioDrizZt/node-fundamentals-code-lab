7c844ef020c522a3fc1e297b26329638c767d85c
Fabio D. Argañaraz <ing.fabio.arg@gmail.com>
2024-03-02 20:09:04 -0300

    ejercicio realiza2

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
-function getRandomNumber() {}
+function getRandomNumber() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const randomNumber = Math.floor(Math.random() * 11); // Genera un número aleatorio entre 0 y 10
+            resolve(randomNumber); // Resuelve la promesa con el número aleatorio generado
+        }, 3000); // Simula un proceso largo que demora 3 segundos
+    });
+}
+
+
+// Ejemplo de uso:
+getRandomNumber()
+    .then(number => {
+        console.log('Número aleatorio:', number);
+    })
+    .catch(error => {
+        console.error('Ocurrió un error:', error);
+    });
 
 module.exports = getRandomNumber;
